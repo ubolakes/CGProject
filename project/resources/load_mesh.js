de9f2512fa9a6fe32d4197ddb432a623906c1961
Umberto Laghi <ul2509@gmail.com>
2024-06-12 09:00:27 +0200

    went back to unedited version

--- project/resources/load_mesh.js
+++ project/resources/load_mesh.js
@@ -1,8 +1,74 @@
-// function to load a texture
-function loadTexture(gl, path, fileName) {
+/*========== Loading and storing the geometry ==========*/
+async function LoadMesh(gl,mesh) {
+   await retrieveDataFromSource(mesh);
+
+   mesh.textures =[];
+
+   let baseHref = mesh.sourceMesh;
+   const r = /[^\/]*$/;
+   baseHref = baseHref.replace(r, '');
+
+   mesh.textures = {
+       defaultWhite: create1PixelTexture(gl, [255, 255, 255, 255]),
+   };
+
+   for (const material of Object.values(mesh.materials)) {
+       for (const [key1, filename] of Object.entries(material)
+           .filter(([key]) => key.endsWith('Map'))) {
+               let texture = mesh.textures[filename];
+               if (!texture) {
+                   const textureHref = baseHref + filename
+                   texture = await createTexture(gl, textureHref).catch(error => alert(error.message));
+                   mesh.textures[filename] = texture;
+
+               }
+               material[key1] = texture;
+           }
+   }
+
+   await Promise.resolve(mesh)
+}
+
+
+//Funzione che serve per recuperare i dati della mesh da un file OBJ
+async function retrieveDataFromSource(mesh){
+   await loadMeshFromOBJ(mesh);
+   if(mesh.fileMTL) {
+       await readMTLFile(mesh.fileMTL, mesh.data);
+
+       mesh.materials = mesh.data.materials;
+       delete mesh.data.materials;
+   }
+}
+
+
+
+//Funzione che utilizza la libreria glm_utils per leggere un file OBJ
+//contenente la definizione della mesh
+async function loadMeshFromOBJ(mesh) {
+   return $.ajax({
+       type: "GET",
+       url: mesh.sourceMesh,
+       dataType: "text",
+       async: false,
+       success: parseobjFile,
+       error: handleError,
+   });
+
+   function parseobjFile(result){
+       mesh.data = parseOBJ(result)
+   }
+
+   function handleError(jqXhr, textStatus, errorMessage){
+       console.error('Error : ' + errorMessage);
+   }
+}
+
+
+//Funzione che carica una texture
+async function loadTexture(gl, path, fileName) {
    const texture = gl.createTexture();
    gl.bindTexture(gl.TEXTURE_2D, texture);
-
    const level = 0;
    const internalFormat = gl.RGBA;
    const width = 1;
@@ -12,161 +78,305 @@ function loadTexture(gl, path, fileName) {
    const srcType = gl.UNSIGNED_BYTE;
    const pixel = new Uint8Array([255, 255, 255, 255]);  // opaque blue
    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-            width, height, border, srcFormat, srcType, pixel);
-   
+       width, height, border, srcFormat, srcType, pixel);
+
    if(fileName){
-      const image = new Image();
-      image.onload = function() {
-         gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-         gl.bindTexture(gl.TEXTURE_2D, texture);
-         gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,srcFormat, srcType, image);
-         if (isPowerOf2(image.width) && isPowerOf2(image.height)) 
+       const image = new Image();
+       image.onload = function() {
+           gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+           gl.bindTexture(gl.TEXTURE_2D, texture);
+           gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,srcFormat, srcType, image);
+           if (isPowerOf2(image.width) && isPowerOf2(image.height))
                gl.generateMipmap(gl.TEXTURE_2D); // Yes, it's a power of 2. Generate mips.
-         else {
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-         }
-      };
-      image.src = path + fileName;
+           else {
+               gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+               gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+               gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+               gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+           }
+       };
+       image.src = path + fileName;
    }
    return texture;
-   
-   function isPowerOf2(value) {
-      return (value & (value - 1)) == 0;
-   }
 }
 
-// function to read an eventually associated MTL file
-function readMTLFile(MTLfileName, mesh){
+//Funzione che utilizza la libreria glm_utils per leggere un eventuale 
+//file MTL associato alla mesh
+async function readMTLFile(MTLfileName, mesh){
    return $.ajax({
-      type: "GET",
-      url: MTLfileName,
-      dataType: "text",
-      async: false,
-      success: parseMTLFile,
-      error: handleError,
+       type: "GET",
+       url: MTLfileName,
+       dataType: "text",
+       async: false,
+       success: parseMTLFile,
+       error: handleError,
    });
-   function parseMTLFile(result, status, xhr){
-      glmReadMTL(result, mesh);
+   function parseMTLFile(result){
+       //glmReadMTL(result, mesh);
+       mesh.materials = parseMTL(result);
    }
    function handleError(jqXhr, textStatus, errorMessage){
-      console.error('Error : ' + errorMessage);
+       console.error('Error : ' + errorMessage);
    }
 }
 
-// function to load data from OBJ file
-function retrieveDataFromSource(mesh){
-   loadMeshFromOBJ(mesh);
-   if(mesh.fileMTL) {
-      readMTLFile(mesh.sourceMesh.substring(0, mesh.sourceMesh.lastIndexOf("/")+1) + mesh.fileMTL, mesh.data); 
-      mesh.materials = mesh.data.materials;
-      delete mesh.data.materials;
-   }
-}
 
-// function to read the content of an OBJ file
-function loadMeshFromOBJ(mesh) {
+async function getMTLFile(MTLfileName, mesh){
    return $.ajax({
-      type: "GET",
-      url: mesh.sourceMesh,
-      dataType: "text",
-      async: false,
-      success: parseobjFile,
-      error: handleError,
+       type: "GET",
+       url: MTLfileName,
+       dataType: "text",
+       async: false,
+       success: parseMTLFile,
+       error: handleError,
    });
-   function parseobjFile(result, status, xhr){                  
-      var result = glmReadOBJ(result,new subd_mesh());
-// uncomment to use LoadSubdivMesh
-//         mesh.data = LoadSubdivMesh(result.mesh);
-      mesh.data = result.mesh;
-      mesh.fileMTL = result.fileMtl;
-   }	
+   function parseMTLFile(result){
+       let mtl = parseMTL(result)
+
+   }
    function handleError(jqXhr, textStatus, errorMessage){
-      console.error('Error : ' + errorMessage);
+       console.error('Error : ' + errorMessage);
    }
-} 
+}
 
-/*========== Loading and storing the geometry ==========*/
-function LoadMesh(gl,mesh) {
 
-   retrieveDataFromSource(mesh);
-   Unitize(mesh.data);
-   // loading texture associated
-   var map = mesh.materials[1].parameter;
-   var path = mesh.sourceMesh.substring(0, mesh.sourceMesh.lastIndexOf("/")+1);
-   //map.set("map_Kd", loadTexture(gl, path, map.get("map_Kd")));
-   var texture = loadTexture(gl, path, map.get("map_Kd"));
+function parseMTL(text) {
+   const materials = {};
+   let material;
 
-   var x=[], y=[], z=[];
-   var xt=[], yt=[];
-   var i0,i1,i2;
-   var nvert=mesh.data.nvert;
-   var nface=mesh.data.nface;
-   var ntexcoord=mesh.data.textCoords.length;
-   // output parameters
-   var positions = [];
-   var normals = [];
-   var texcoords = [];
-   var numVertices;
-   var ambient, diffuse, specular, emissive, shininess, opacity;
-
-   for (var i=0; i<nvert; i++){
-      x[i]=mesh.data.vert[i+1].x;
-      y[i]=mesh.data.vert[i+1].y;
-      z[i]=mesh.data.vert[i+1].z;       
-      }
-   for (var i=0; i<ntexcoord-1; i++){
-      xt[i]=mesh.data.textCoords[i+1].u;
-      yt[i]=mesh.data.textCoords[i+1].v;      
-   }
-   for (var i=1; i<=nface; i++){
-      i0=mesh.data.face[i].vert[0]-1;
-      i1=mesh.data.face[i].vert[1]-1;
-      i2=mesh.data.face[i].vert[2]-1;
-      positions.push(x[i0],y[i0],z[i0],x[i1],y[i1],z[i1],x[i2],y[i2],z[i2]); 
-      i0=mesh.data.facetnorms[i].i;
-      i1=mesh.data.facetnorms[i].j;
-      i2=mesh.data.facetnorms[i].k;
-      normals.push(i0,i1,i2,i0,i1,i2,i0,i1,i2); 
-      i0=mesh.data.face[i].textCoordsIndex[0]-1;
-      i1=mesh.data.face[i].textCoordsIndex[1]-1;
-      i2=mesh.data.face[i].textCoordsIndex[2]-1;
-      texcoords.push(xt[i0],yt[i0],xt[i1],yt[i1],xt[i2],yt[i2]);
-   }         
-   numVertices=3*nface;
-
-   if (mesh.fileMTL == null){
-      ambient=mesh.materials[0].parameter.get("Ka");
-      diffuse=mesh.materials[0].parameter.get("Kd");
-      specular=mesh.materials[0].parameter.get("Ks");
-      emissive=mesh.materials[0].parameter.get("Ke");
-      shininess=mesh.materials[0].parameter.get("Ns");
-      opacity=mesh.materials[0].parameter.get("Ni");
-   }
-   else{
-      ambient=mesh.materials[1].parameter.get("Ka");
-      diffuse=mesh.materials[1].parameter.get("Kd");
-      specular=mesh.materials[1].parameter.get("Ks");
-      emissive=mesh.materials[1].parameter.get("Ke");
-      shininess=mesh.materials[1].parameter.get("Ns");
-      opacity=mesh.materials[1].parameter.get("Ni");
-   }
-
-   // outputting parameters for each mesh as an object
-   return {
-      positions: positions,
-      normals: normals,
-      texcoords: texcoords,
-      numVertices: numVertices,
-      texture: texture,
-      ambient: ambient,
-      diffuse: diffuse,
-      specular: specular,
-      emissive: emissive,
-      shininess: shininess,
-      opacity: opacity,
+   const keywords = {
+       newmtl(parts, unparsedArgs) {
+           material = {};
+           materials[unparsedArgs] = material;
+       },
+       /* eslint brace-style:0 */
+       Ns(parts)       { material.shininess      = parseFloat(parts[0]); },
+       Ka(parts)       { material.ambient        = parts.map(parseFloat); },
+       Kd(parts)       { material.diffuse        = parts.map(parseFloat); },
+       Ks(parts)       { material.specular       = parts.map(parseFloat); },
+       Ke(parts)       { material.emissive       = parts.map(parseFloat); },
+       map_Kd(parts, unparsedArgs)   { material.diffuseMap = unparsedArgs; },
+       map_Ns(parts, unparsedArgs)   { material.specularMap = unparsedArgs; },
+       map_Bump(parts, unparsedArgs) { material.normalMap = unparsedArgs; },
+       Ni(parts)       { material.opticalDensity = parseFloat(parts[0]); },
+       d(parts)        { material.opacity        = parseFloat(parts[0]); },
+       illum(parts)    { material.illum          = parseInt(parts[0]); },
    };
 
+   const keywordRE = /(\w*)(?: )*(.*)/;
+   const lines = text.split('\n');
+   for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
+       const line = lines[lineNo].trim();
+       if (line === '' || line.startsWith('#')) {
+           continue;
+       }
+       const m = keywordRE.exec(line);
+       if (!m) {
+           continue;
+       }
+       const [, keyword, unparsedArgs] = m;
+       const parts = line.split(/\s+/).slice(1);
+       const handler = keywords[keyword];
+       if (!handler) {
+           console.warn('unhandled keyword:', keyword);  // eslint-disable-line no-console
+           continue;
+       }
+       handler(parts, unparsedArgs);
+   }
+
+   return materials;
+}
+
+function create1PixelTexture(gl, pixel) {
+   const texture = gl.createTexture();
+   gl.bindTexture(gl.TEXTURE_2D, texture);
+   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+       new Uint8Array(pixel));
+   return texture;
+}
+
+async function createTexture(gl, url) {
+   const texture = create1PixelTexture(gl, [128, 192, 255, 255]);
+   // Asynchronously load an image
+   const image = new Image();
+   image.src = url;
+   image.addEventListener('load', function() {
+       // Now that the image has loaded make copy it to the texture.
+       gl.bindTexture(gl.TEXTURE_2D, texture);
+       gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
+
+       // Check if the image is a power of 2 in both dimensions.
+       if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+           // Yes, it's a power of 2. Generate mips.
+           gl.generateMipmap(gl.TEXTURE_2D);
+       } else {
+           // No, it's not a power of 2. Turn of mips and set wrapping to clamp to edge
+           gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+           gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+           gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+       }
+   });
+   return texture;
+}
+
+function parseOBJ(text) {
+   // because indices are base 1 let's just fill in the 0th data
+   const objPositions = [[0, 0, 0]];
+   const objTexcoords = [[0, 0]];
+   const objNormals = [[0, 0, 0]];
+   const objColors = [[0, 0, 0]];
+
+   // same order as `f` indices
+   const objVertexData = [
+       objPositions,
+       objTexcoords,
+       objNormals,
+       objColors,
+   ];
+
+   // same order as `f` indices
+   let webglVertexData = [
+       [],   // positions
+       [],   // texcoords
+       [],   // normals
+       [],   // colors
+   ];
+
+   const materialLibs = [];
+   const geometries = [];
+   let geometry;
+   let groups = ['default'];
+   let material = 'default';
+   let object = 'default';
+
+   const noop = () => {};
+
+   function newGeometry() {
+       if (geometry && geometry.data.position.length) {
+           geometry = undefined;
+       }
+   }
+
+   function setGeometry() {
+       if (!geometry) {
+           const position = [];
+           const texcoord = [];
+           const normal = [];
+           const color = [];
+           webglVertexData = [
+               position,
+               texcoord,
+               normal,
+               color,
+           ];
+           geometry = {
+               object,
+               groups,
+               material,
+               data: {
+                   position,
+                   texcoord,
+                   normal,
+                   color,
+               },
+           };
+           geometries.push(geometry);
+       }
+   }
+
+   function addVertex(vert) {
+       const ptn = vert.split('/');
+       ptn.forEach((objIndexStr, i) => {
+           if (!objIndexStr) {
+               return;
+           }
+           const objIndex = parseInt(objIndexStr);
+           const index = objIndex + (objIndex >= 0 ? 0 : objVertexData[i].length);
+           webglVertexData[i].push(...objVertexData[i][index]);
+           // if this is the position index (index 0) and we parsed
+           // vertex colors then copy the vertex colors to the webgl vertex color data
+           if (i === 0 && objColors.length > 1) {
+               geometry.data.color.push(...objColors[index]);
+           }
+       });
+   }
+
+   const keywords = {
+       v(parts) {
+           // if there are more than 3 values here they are vertex colors
+           if (parts.length > 3) {
+               objPositions.push(parts.slice(0, 3).map(parseFloat));
+               objColors.push(parts.slice(3).map(parseFloat));
+           } else {
+               objPositions.push(parts.map(parseFloat));
+           }
+       },
+       vn(parts) {
+           objNormals.push(parts.map(parseFloat));
+       },
+       vt(parts) {
+           // should check for missing v and extra w?
+           objTexcoords.push(parts.map(parseFloat));
+       },
+       f(parts) {
+           setGeometry();
+           const numTriangles = parts.length - 2;
+           for (let tri = 0; tri < numTriangles; ++tri) {
+               addVertex(parts[0]);
+               addVertex(parts[tri + 1]);
+               addVertex(parts[tri + 2]);
+           }
+       },
+       s: noop,    // smoothing group
+       mtllib(parts, unparsedArgs) {
+           // the spec says there can be multiple filenames here
+           // but many exist with spaces in a single filename
+           materialLibs.push(unparsedArgs);
+       },
+       usemtl(parts, unparsedArgs) {
+           material = unparsedArgs;
+           newGeometry();
+       },
+       g(parts) {
+           groups = parts;
+           newGeometry();
+       },
+       o(parts, unparsedArgs) {
+           object = unparsedArgs;
+           newGeometry();
+       },
+   };
+
+   const keywordRE = /(\w*)(?: )*(.*)/;
+   const lines = text.split('\n');
+   for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
+       const line = lines[lineNo].trim();
+       if (line === '' || line.startsWith('#')) {
+           continue;
+       }
+       const m = keywordRE.exec(line);
+       if (!m) {
+           continue;
+       }
+       const [, keyword, unparsedArgs] = m;
+       const parts = line.split(/\s+/).slice(1);
+       const handler = keywords[keyword];
+       if (!handler) {
+           console.warn('unhandled keyword:', keyword);  // eslint-disable-line no-console
+           continue;
+       }
+       handler(parts, unparsedArgs);
+   }
+
+   // remove any arrays that have no entries.
+   for (const geometry of geometries) {
+       geometry.data = Object.fromEntries(
+           Object.entries(geometry.data).filter(([, array]) => array.length > 0));
+   }
+
+   return {
+       geometries,
+       materialLibs,
+   };
 }
