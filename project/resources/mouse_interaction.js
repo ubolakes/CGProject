8d436abc791d017123524e52b63976ee6b1bb77c
Umberto Laghi <ul2509@gmail.com>
2024-06-20 14:59:38 +0200

    moved mouse interactions to a specific file

---
+++ project/resources/mouse_interaction.js
@@ -0,0 +1,78 @@
+/*
+Author: Umberto Laghi
+E-mail: umberto.laghi@studio.unibo.it
+Github: @ubolakes
+*/
+
+/* this file contains the functions used to manage mouse interaction on canvas */
+
+// defining constants and variables
+//const max_zoom = 100;
+//const min_zoom = 1;
+var drag = false;
+var old_x, oldy;
+var zoom_enabled = false;
+var dX, dY;
+
+// linking event listeners to handlers
+function add_mouse_interaction(scene) {
+    scene.canvas.onmousedown = mouseDown;
+    scene.canvas.onmouseup = mouseUp;
+    scene.canvas.mouseout = mouseOut;
+    scene.canvas.onmouseover = mouseOver;
+    scene.canvas.onmousemove = mouseMove;
+    scene.canvas.onwheel = onWheel;
+}
+
+// pressing mouse button
+var mouseDown = function(e){
+    drag = true;
+    old_x = e.pageX, old_y = e.pageY;
+    e.preventDefault();
+    return false;
+};
+
+// releasing mouse button
+var mouseUp = function(e) {
+    // disabling drag
+    drag = false;
+};
+
+// moving cursor out
+var mouseOut = function(e) {
+    // disabling drag
+    drag = false
+    // disabling zoom
+    zoom_enabled = false;
+    //console.log("Zoom enabled: "+ zoom_enabled);
+}
+
+// moving cursor
+var mouseMove = function(e) {
+    if (!drag)
+        return false;
+    // computing deltas on both axes
+    dX = (e.pageX - old_x) * 2 * Math.PI / canvas.width;
+    dY = (e.pageY - old_y) * 2 * Math.PI / canvas.height;
+    // updating camera parameters
+    scene.camera.pan(-dX * 0.2);
+    scene.camera.tilt(-dY * 0.2);
+    // updating old params
+    old_x = e.pageX, old_y = e.pageY;
+    e.preventDefault();
+};
+
+// mouse on canvas
+var mouseOver = function(e) {
+    // enabling zoom in and out
+    zoom_enabled = true;
+}
+
+// using wheel to zoom 
+var onWheel = function(e) {
+    if (!zoom_enabled)
+        return false;
+    // getting closer or further
+    scene.camera.dolly(-e.deltaY/100);
+    e.preventDefault();
+};
