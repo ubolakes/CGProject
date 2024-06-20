32062d80f4791809d201e238c37efb57a1d08ff7
Umberto Laghi <ul2509@gmail.com>
2024-06-20 19:24:30 +0200

    added pinch to zoom gesture

--- project/resources/touch_interaction.js
+++ project/resources/touch_interaction.js
@@ -6,14 +6,22 @@ Github: @ubolakes
 
 /* this file contains the functions used to manage touch interaction on canvas */
 
-// defining variables
+// defining constant and variables
+// single touch point
 const max_zoom = 100;
 const min_zoom = 1;
-var drag = false;
 var old_x, oldy;
 var zoom_enabled = false;
 var dX, dY;
+// two touch points
+var old_x0, old_y0;
+var old_x1, old_y1;
+var dX0, dY0;
+var dX1, dY1;
+var m;
+var old_dist, new_dist;
 
+var touches;
 // linking event listeners to handlers
 function add_touch_interaction(scene) {
     scene.canvas.ontouchstart = touchStart;
@@ -23,33 +31,66 @@ function add_touch_interaction(scene) {
 }
 
 var touchStart = function(e) {
-    const touch = e.touches[0]; // getting the touch object
-    drag = true;
-    old_x = touch.clientX, old_y = touch.clientY;
     e.preventDefault();
-    return false;
+    // counting how many points
+    if (e.touches.length > 1){
+        // setting old values
+        old_x0 = e.touches[0].clientX;
+        old_y0 = e.touches[0].clientY;
+        old_x1 = e.touches[1].clientX;
+        old_y1 = e.touches[1].clientY;
+        // computing old distance
+        old_dist = get_2D_distance(old_x0, old_y0, old_x1, old_y1);
+        //console.log(old_dist);
+    } else { // just one touch point
+        const touch = e.touches[0]; // getting the touch object
+        old_x = touch.clientX, old_y = touch.clientY;
+        return false;
+    }
 }
 
 var touchMove = function(e) {
-    if (!drag)
-        return false;
+    e.preventDefault();
+    // two touch points
+    if (e.touches.length > 1){
+        pinch(e);
+        //old_dist = new_dist;
+    } else {
+    // just one touch point
     // getting touch object
     const touch = e.touches[0];
-    // computing deltas on both axes
-    dX = (touch.clientX - old_x) * 2 * Math.PI / canvas.width;
-    dY = (touch.clientY - old_y) * 2 * Math.PI / canvas.height;
+    // computing deltas on both aces
+    dX = (touch.clientX - old_x) * 2 * Math.PI / scene.canvas.width;
+    dY = (touch.clientY - old_y) * 2 * Math.PI / scene.canvas.height;
     // updating camera parameters
-    scene.camera.pan(-dX * 0.2);
-    scene.camera.tilt(-dY * 0.2);
-    // updating old params
+    scene.camera.pan(dX * 0.2);
+    scene.camera.tilt(dY * 0.2);
+    // updating old parameters
     old_x = touch.clientX, old_y = touch.clientY;
-    e.preventDefault();
+    }
 }
 
 var touchEnd = function(e) {
-    drag = false;
+    //console.log("touch end");
 }
 
 var touchCancel = function(e) {
-    drag = false;
+    //console.log("touch cancel");
 }
+
+// pinch to zoom
+function pinch(e) {
+    e.preventDefault();
+    let step = 0.05;
+    // getting touch objects
+    const touch0 = e.touches[0];
+    const touch1 = e.touches[1];
+    // computing the distance between points
+    new_dist = get_2D_distance(touch0.clientX, touch0.clientY, touch1.clientX, touch1.clientY);
+    // computing the difference
+    diff = old_dist - new_dist;
+    // getting closer or further depending on diff sign
+    scene.camera.dolly(step * Math.sign(-diff));
+    // assigning old_dist
+    old_dist = new_dist;
+}  
